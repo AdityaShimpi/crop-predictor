e7323b9011b16855b0f879bb33875304dd3f977d
AdityaShimpi <98681601+AdityaShimpi@users.noreply.github.com>
2024-08-01 21:22:33 +0530

    Update app.py for deployment purpose

--- app.py
+++ app.py
@@ -150,5 +150,5 @@ def predict():
             traceback.print_exc()  # Print detailed exception traceback
             return jsonify({'error': 'An error occurred. Please try again later.'}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
